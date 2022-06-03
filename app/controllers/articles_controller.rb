c78fb9964b3b02b26b5b0c3a68653c4a6f3ea847
Avishek Kumar <avishekkumar@4409b-avishek.blrgle.in>
2022-06-03 17:41:21 +0530

    Added articles table

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+
+    def index
+        @articles=Article.all
+    end
+
+
+    def show
+        @article=Article.find(params["id"])
+    end
+
+
+    def new
+        @article=Article.new();
+    end
+
+
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+
+
+    def create
+        @article=Article.new(params.require(:article).permit(:title,:desc));
+        if @article.save
+            flash[:notice] = "This article was saved successfully"
+            redirect_to article_path(@article)
+        else 
+            render "new"
+        end
+    end
+
+
+
+    def update
+        @article=Article.find(params[:id])
+        
+        if @article.update(params.require(:article).permit(:title,:desc))
+            flash[:notice] = "This article was updated successfully"
+            redirect_to article_path(@article)
+        else 
+            
+            render "edit"
+        end
+    end
+
+
+    def destroy
+        @article=Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+end
