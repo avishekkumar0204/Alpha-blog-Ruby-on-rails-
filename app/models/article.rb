b2f5f42fdf8fcc6d89c91206f6e10211f3da29a5
Avishek Kumar <avishek.kumar1@greatlearning.in>
2022-06-06 11:17:07 +0530

    add association between user and article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
+    
     validates :title, presence: true, length: {minimum: 5, maximum: 50}
     validates :desc, presence: true, length: {minimum: 8, maximum: 500}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+    has_many :articles
     validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum:25}
     validates :email, presence: true, uniqueness: true ,length: {maximum:25}, format: { with: /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/ }
 end
